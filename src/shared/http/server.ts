096d752e7d1b44ce762a42e8b6f6fb0809f6b2b4
Carlos Castro <carloscastrogames@gmail.com>
2021-07-15 08:48:02 -0300

    projeto ok

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,3 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
 
+const app = express();
 
-console.log("Hello Dev");
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('Server ON port 333');
+});
