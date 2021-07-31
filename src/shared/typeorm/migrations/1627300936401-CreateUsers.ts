05af31897e7be578dc4b4596f5d6e3b6c2bdb0ec
Carlos Castro <carloscastrogames@gmail.com>
2021-07-31 15:06:52 -0300

    🗃 Migration: Implements MigrationCreateUsers

---
+++ src/shared/typeorm/migrations/1627300936401-CreateUsers.ts
@@ -0,0 +1,52 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUsers1627300936401 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            isPrimary: true,
+            name: 'uuid',
+            type: 'uuid',
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+          },
+          {
+            name: 'avatar',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
