178482acecd96c7264ccde39771e58f3e1802a6a
Carlos Castro <carloscastrogames@gmail.com>
2021-08-02 08:30:23 -0300

    🚚 Controller: update AuthenticateUserService now returm Token

--- src/modules/users/services/AuthenticateUserServices.ts
+++ src/modules/users/services/AuthenticateUserServices.ts
@@ -1,5 +1,6 @@
 import AppError from '@shared/http/errors/AppError';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
@@ -11,10 +12,11 @@ interface IRequest {
 
 interface IResponse {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
-  public async execute({ email, password }: IRequest): Promise<User> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UsersRepository);
 
     const user = await usersRepository.findByEmail(email);
@@ -29,7 +31,15 @@ class AuthenticateUserService {
       throw new AppError('Incorrect email/password combination.', 401);
     }
 
-    return user;
+    const token = sign({}, '23ea5153f6b54710ebd24ecb5de7580d', {
+      subject: user.id,
+      expiresIn: '7d',
+    });
+
+    return {
+      user,
+      token,
+    };
   }
 }
 
